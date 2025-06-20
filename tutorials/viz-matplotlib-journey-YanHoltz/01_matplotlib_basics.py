a99cb10249002847ff4e6d4a596e6bbb1730d5e6
Cedric Vidonne <cedric@vidonne.me>
2025-06-20 14:26:08 +0200

    add lessos 01_3-4

---
+++ tutorials/viz-matplotlib-journey-YanHoltz/01_matplotlib_basics.py
@@ -0,0 +1,57 @@
+### 01 Visualizing data with python
+
+# lots of tools but matplotlib a clear winner in popularity
+
+### 02 Matplotlib pattern
+
+# import needed package
+import matplotlib.pyplot as plt
+
+# Initialize the chart with subplots
+fig, ax = plt.subplots()
+# fill the axes
+ax.plot([1,2,3,4,5], [2,3,1,4,5])
+# show the plot
+plt.show()
+
+
+# scatter example
+fig, ax = plt.subplots()
+ax.scatter([1,2,3,4,5], [2,3,1,4,5])
+plt.show()
+
+### 03 Pyplot VS axes
+
+# pyplot interface
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4,5], [2,3,1,4,5])
+plt.show()
+
+# Axes interface is the way to go
+
+### 04 What on earth are Figure, Axes, and Subplot?!
+
+# Axes
+# The Axes is where the actual data visualization happens—it contains the plot elements such as lines, bars, scatter points, and more.
+
+# Figure
+# A Figure is the top-level container for everything in a Matplotlib chart. Think of it as the canvas where all elements are drawn, including one or more Axes.
+# One figure can have several axes
+
+# Adding text
+# It requires at least three arguments: x: the position on the x-axis, y: the position on the y-axis, s: the text content
+# ax.text(x, y, s): adds text to the Axes
+# fig.text(x, y, s): adds text to the Figure
+
+# Axes and Figure: Different Coordinate Systems 
+# Axes use the same coordinate system as the data (i.e., data coordinates).
+# Figure coordinates range from 0 to 1 on both the x and y axes.
+
+# Conclusion
+# Every Matplotlib visualization is a Figure with one or more Axes.
+# A Figure is the overall container for all elements, including Axes and other graph components. Its coordinates range from 0 to 1 on both x and y axes.
+# An Axes is a container for displaying a single chart (line chart, bar plot, etc.), with coordinates in the same scale as the data.
+# When a Figure has a single Axes (as in every chart we've made so far), the center of the Axes and the Figure align.
+
+### 05 Build (almost) any chart type
